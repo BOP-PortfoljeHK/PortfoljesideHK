74ae33eea5a0b352b6c3928239ac75dc38418fc6
Sara Ørjansen <sara.orj@gmail.com>
2026-02-17 12:03:35 +0100

    La til flere schemas

---
+++ portefoljeside-hk/schemaTypes/appliedArts.ts
@@ -0,0 +1,42 @@
+import { defineType, defineField } from "sanity";
+
+export const appliedArts = defineType({
+    name: 'appliedArts',
+    title: 'Applied Arts',
+    type: 'document',   
+    fields: [
+        defineField({
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+            validation: Rule => Rule.required(), // gjør at feltet er obligatorisk å fylle ut
+        }),
+        defineField({
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title', // genererer slug basert på title-feltet
+            }
+        }),
+        defineField({
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+        }),
+        defineField({
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true, // gjør det mulig å velge et fokuspunkt i bildet
+            },
+        }),
+        defineField({
+            name: 'gallery',
+            title: 'Gallery',
+            type: 'array',
+            of: [{ type: 'image' }], // lar deg legge til flere bilder i galleriet
+        }),
+    ]
+})
--- portefoljeside-hk/schemaTypes/index.ts
+++ portefoljeside-hk/schemaTypes/index.ts
@@ -1,1 +1,12 @@
-export const schemaTypes = []
+// Denne filen brukes for å eksportere alle schema-typene som skal brukes i Sanity.
+// Dvs. alle schemas som lages må legges inn her!
+
+import { about } from './about'
+import { contact } from './contact'
+import { oyaglass } from './oyaglass'
+import { jewelrycollection } from './jewelrycollection'
+import { appliedArts } from './appliedArts'
+
+
+export const schemaTypes = [ jewelrycollection, appliedArts, oyaglass, about, contact, ]
+
