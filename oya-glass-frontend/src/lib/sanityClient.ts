0121a1d51c65b9a5de6bcbad28ca978bd5b30bcf
Vibeke Wolden <vibwol@live.no>
2026-02-24 11:09:13 +0100

    public
    Endret sanityClient til å følge standarden i ASTRO og bruke PUBLIC og ikke VITE

--- oya-glass-frontend/src/lib/sanityClient.ts
+++ oya-glass-frontend/src/lib/sanityClient.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@sanity/client'
 
 export const sanity = createClient({
-    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-    dataset: import.meta.env.VITE_SANITY_DATASET,
-    apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
+    projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+    dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+    apiVersion: import.meta.env.PUBLIC_SANITY_API_VERSION,
     useCdn: true, 
 })
